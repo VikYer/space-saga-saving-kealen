e878e056b6a4f324e603a94232fda5feecc87d35
VikYer <piligrimxix@gmail.com>
2025-08-24 13:32:17 +0200

    refactor(command handler): use dict.get with fallbacks to prevent KeyError in option handling

--- gui.py
+++ gui.py
@@ -62,15 +62,18 @@ class SpaceSaga(App):
         yield Footer()
 
     def on_mount(self) -> None:
-        self.show_location('Corn Farm')
+        self.show_location('Corn farm')
 
     def show_location(self, location_name: str) -> None:
         """Display location description in quest-text and available commands in command-panel"""
         self.current_location = location_name
         self.options_stack = []
 
-        location = self.locations[location_name]
-        self.quest_text.update(location['description'])
+        location = self.locations.get(location_name)
+        if not location:
+            return
+
+        self.quest_text.update(location.get('description'))
 
         options = location.get('options')
         self._show_options(options)
@@ -80,17 +83,23 @@ class SpaceSaga(App):
         """Helper: display command options in command-panel."""
         self.command_panel.clear_options()
         for opt_id, opt in options.items():
-            self.command_panel.add_option(Option(opt['text'], opt_id))
+            self.command_panel.add_option(Option(opt.get('text'), opt_id))
         self.set_focus(self.command_panel)
         self.command_panel.highlighted = 0
 
     def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
         """Handle option selection from command-panel."""
-        location = self.locations[self.current_location]
+        location = self.locations.get(self.current_location)
+        if not location:
+            return
+
         options = location.get('options')
 
         for el in self.options_stack:
-            options = options[el].get('options')
+            parent = options.get(el)
+            if not parent:
+                return
+            options = parent.get('options')
 
         option = options.get(event.option_id)
         if not option:
@@ -115,18 +124,17 @@ class SpaceSaga(App):
                 parent_option = None
 
                 for el in self.options_stack:
-                    parent_option = options[el]
+                    parent_option = options.get(el)
                     options = parent_option.get('options')
 
                 if parent_option and 'description' in parent_option:
                     self.quest_text.update(parent_option['description'])
                 else:
-                    self.quest_text.update(location['description'])
+                    self.quest_text.update(location.get('description'))
 
                 self._show_options(options)
 
             else:
-                self.quest_text.update(location["description"])
+                self.quest_text.update(location.get("description"))
                 self._show_options(location.get('options'))
             return
-
